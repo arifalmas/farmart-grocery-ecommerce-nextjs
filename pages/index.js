0d1504cadcc996313e03dfeab522ff2a0a40bbf5
coderarife <freelancerarifar@gmail.com>
2022-09-04 23:08:12 +0600

    commit message

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-
-
+import HomeHero from "../app/components/Home/HomeHero";
+import HomeCategory from "../app/components/Home/HomeCategory";
+import FeaturedBrands from "../app/components/Home/FeaturedBrands";
 export default function Home() {
   return (
     <>
-   
+      <HomeHero/>
+      <HomeCategory />
+      <FeaturedBrands/>
     </>
   )
 }
